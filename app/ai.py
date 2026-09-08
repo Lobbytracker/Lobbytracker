a3e86d4f3ec7e6e124900979b53617f2f3ddd8d6
Heljä Lehtinen <lehthelj@gmail.com>
2026-09-08 14:23:22 +0300

    added openai stuff using aitta

---
+++ app/ai.py
@@ -0,0 +1,17 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+key = os.getenv('AITTA_API_KEY')
+
+url = "https://aitta-api.csc.fi/openai/v1"
+
+client = openai.OpenAI(api_key=key, base_url=url)
+chat_completion = client.chat.completions.create(messages=[{"role":"user",
+                                                            "content":"count to ten backwords"}], model="openai/gpt-oss-120b", stream=True)
+
+for chunk in chat_completion:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end='', flush=True)
